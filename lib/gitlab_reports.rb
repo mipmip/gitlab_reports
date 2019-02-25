17f1c72d66cdbdab44f46d8eabd6bce4b2d10ea6
Pim Snel <pim@lingewoud.nl>
2019-02-25 16:11:27 +0100

    initial working version

---
+++ lib/gitlab_reports.rb
@@ -0,0 +1,6 @@
+require "gitlab_reports/version"
+
+module GitlabReports
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gitlab_reports/version.rb
@@ -0,0 +1,3 @@
+module GitlabReports
+  VERSION = "0.1.0"
+end
